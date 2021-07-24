069d87f47132d4632e04273581eb9ba80383c6de
Nebel-hb <nebel.hb@gmail.com>
2021-07-24 13:45:12 +0900

    Java_10

---
+++ Java_10/Hero.java
@@ -0,0 +1,11 @@
+public class Hero{
+  String name = "ミナト";
+  int hp = 100;
+
+  // public void attack(Matago m){
+  //   System.out.println("5ポイントのダメージを与えた！");
+  // }
+  public void run(){
+    System.out.println(this.name + "は逃げ出した！");
+  }
+}
