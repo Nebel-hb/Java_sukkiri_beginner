fd9e0c3ca6a6432c675d091d4dff9c5856f36ab8
Nebel-hb <nebel.hb@gmail.com>
2021-07-28 15:44:17 +0900

    Java_14

---
+++ Java_14/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main (String[] args){
+    Hero h1 = new Hero();
+    Hero h2 = new Hero();
+    System.out.println(Hero.money);
+    System.out.println(h1.money);
+    h1.money = 300;
+    System.out.println(h2.money);
+  }
+}
