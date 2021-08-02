a66bbff71ac937f8e66f72b8a7ffe78282af8872
Nebel-hb <nebel.hb@gmail.com>
2021-08-02 23:03:26 +0900

    Java_1

--- Java_1/Main.java
+++ Java_1/Main.java
@@ -1,8 +1,14 @@
 public class Main{
   public static void main(String[] args){
-    int a = 3;
-    int b = 5;
-    int c = a * b;
-    System.out.println("面積は" + c);
+    // int a = 3;
+    // int b = 5;
+    // int c = a * b;
+    // System.out.println("面積は" + c);
+
+    boolean result = true;
+    char favoriteCharacter = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた";
   }
 }
