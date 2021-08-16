40381c8e1eb32bb437bf64e0566b5e727b3ec515
Nebel-hb <nebel.hb@gmail.com>
2021-08-16 09:12:16 +0900

    Java_16

--- Java_16/Main.java
+++ Java_16/Main.java
@@ -4,6 +4,13 @@ public class Main {
   public static void main(String[] args) {
     Hero h1 = new Hero("斉藤");
     Hero h2 = new Hero("鈴木");
+    // List<Hero> heroes = new ArrayList<Hero>();
+    // heroes.add(h1);
+    // heroes.add(h2);
+    // for (Hero h : heroes){
+    //   System.out.println(h.getName());
+    // }
+
     Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
     heroes.put(h1, 3);
     heroes.put(h2, 7);
