98f4b53b45954dea8457c403245a47fa7fe78216
Nebel-hb <nebel.hb@gmail.com>
2021-08-21 07:28:59 +0900

    Java_14_try3

--- Java_14/Account.java
+++ Java_14/Account.java
@@ -1,20 +1,23 @@
-public class Account{
+public class Account {
   String accountNumber;
   int balance;
 
   public String toString(){
-    return "¥¥" + this.balance +
-    " (口座番号 ：" + this.accountNumber + ")";
+    return this.balance + "口座番号" + this.accountNumber;
   }
 
   public boolean equals(Object o){
     if (this == o){
       return true;
     }
-    if (o instanceof Account){
-      Account a (Account)o;
+    if ( o instanceof Account){
+      Account a = (account)o;
       String an1 = this.accountNumber.trim();
       String an2 = a.accountNumber.trim();
+
+      //trim はjava.lang.String クラスのメソッドで、
+      //空白を削除してくれる
+      
       if (an1.equals(an2)){
         return true;
       }
