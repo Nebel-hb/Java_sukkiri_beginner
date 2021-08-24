44b48ad2b1d705924dacea05acc6465bc02310fd
Nebel-hb <nebel.hb@gmail.com>
2021-08-24 20:19:26 +0900

    triangle

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle{
+  public static void main(String[] args){
+    String star = "*";
+
+    for (int i = 0; i < 5; i++) {
+      for(int j = 0; j < 5-i; j++) {
+          System.out.print(" ");
+      }
+      for(int k = 0; k <= (i*2); k++) {
+          System.out.print(star);
+      }
+      System.out.println("\n");
+    }
+  }
+}
