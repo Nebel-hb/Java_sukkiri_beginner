6d4ebb175173801b8af7a05755ff9097882d9964
Nebel-hb <nebel.hb@gmail.com>
2021-09-01 18:18:19 +0900

    Practical_edit Java_3

--- Practical_edition/Java_3/Main.java
+++ Practical_edition/Java_3/Main.java
@@ -1,14 +1,23 @@
 import java.util.function.*;
-public class Main{
-  public static Integer len(String s){
-    return s.length();
-  }
-  public static void main(String[] args){
-    Function<String, Integer> func = Main::len;
-    int a = func.apply("Java");
-    System.out.prinln("文字列「Java」は" + a + "文字です。");
-  }
 
-  public static int twice(int x){return x * 2;}
-  public static int sub(int a, int b){return a - b;}
+public class Main{
+  public static void main(String[] args){
+    Function<String, Integer> func = (String s) -> {return s.length(); };
+    int n = func.apply("Java");
+    System.out.println("文字列「Java」は" + n + "文字です");
+  }
 }
+
+// public class Main{
+  // public static Integer len(String s){
+  //   return s.length();
+  // }
+  // public static void main(String[] args){
+  //   Function<String, Integer> func = Main::len;
+  //   int a = func.apply("Java");
+  //   System.out.prinln("文字列「Java」は" + a + "文字です。");
+  // }
+
+  // public static int twice(int x){return x * 2;}
+  // public static int sub(int a, int b){return a - b;}
+// }
