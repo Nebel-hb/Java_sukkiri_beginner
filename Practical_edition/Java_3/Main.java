6226978cd3733edaa4ef5317773419e38d6619a2
Nebel-hb <nebel.hb@gmail.com>
2021-09-02 08:52:20 +0900

    Pratical edit Java_3

--- Practical_edition/Java_3/Main.java
+++ Practical_edition/Java_3/Main.java
@@ -1,10 +1,34 @@
-import java.util.function.*;
+// import java.util.function.*;
+import java.util.*;
+import java.util.stream.*;
 
 public class Main{
   public static void main(String[] args){
-    Function<String, Integer> func = (String s) -> {return s.length(); };
-    int n = func.apply("Java");
-    System.out.println("文字列「Java」は" + n + "文字です");
+    List<String> names = List.of("湊", "朝香", "菅原", "大江");
+    names.stream().filter(n -> n.length() <= 4).map(n -> n + "さん").forEach(System.out::println);
+
+
+    // IntPredicate f1 = x -> x % 2 == 1;
+    // Func2 f2 = (point, name) -> {
+    //   return name + "さんは" + (point > 65 ? "合格" : "不合格");
+    // };
+
+    // Func1 f1 = x -> x % 2 == 1;
+    // Func2 f2 = (point, name) -> {
+    //   return name + "さんは" + (point > 65 ? "合格" : "不合格");
+    // };
+
+    // FuncList funclist = new FuncList();
+    // Func1 f1 = FuncList::isOdd;
+    // Func2 f2 = funclist::passCheck;
+
+    // System.out.println(f1.test(15));
+    // System.out.println(f1.call(15));
+    // System.out.println(f2.call(66, "Smith"));
+
+    // Function<String, Integer> func = (String s) -> {return s.length(); };
+    // int n = func.apply("Java");
+    // System.out.println("文字列「Java」は" + n + "文字です");
   }
 }
 
