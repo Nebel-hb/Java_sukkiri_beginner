810db84ffcd9715b76551ce7a3dcfdfbbf5125b1
Nebel-hb <nebel.hb@gmail.com>
2021-09-02 09:37:57 +0900

    Pratical edit Java_3

--- Practical_edition/Java_3/Main.java
+++ Practical_edition/Java_3/Main.java
@@ -4,7 +4,11 @@ import java.util.stream.*;
 
 public class Main{
   public static void main(String[] args){
-    List<String> names = List.of("湊", "朝香", "菅原", "大江");
+    List<String> names = new ArrayList<>();
+    names.add("湊");
+    names.add("朝香");
+    names.add("菅原");
+    names.add("大江");
     names.stream().filter(n -> n.length() <= 4).map(n -> n + "さん").forEach(System.out::println);
 
 
