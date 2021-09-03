c4c05880d32bc5015bddbebe63f743031c3783f2
Nebel-hb <nebel.hb@gmail.com>
2021-09-03 12:08:00 +0900

    Practical_edition Java_4

---
+++ Practical_edition/Java_4/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args){
+    boolean isErr = true;
+    if (isErr){
+      System.out.println("データが壊れています。異常終了します。");
+      System.exit(1);
+    }
+    System.out.println("正常終了しました。");
+  }
+}
