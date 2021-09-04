4208edf80a5bfbd88b1f1c77808b48d12e2f6139
Nebel-hb <nebel.hb@gmail.com>
2021-09-05 00:01:12 +0900

    Practical_edition Java_4

--- Practical_edition/Java_4/Main.java
+++ Practical_edition/Java_4/Main.java
@@ -1,10 +1,19 @@
 public class Main{
   public static void main(String[] args){
-    boolean isErr = true;
-    if (isErr){
-      System.out.println("データが壊れています。異常終了します。");
-      System.exit(1);
-    }
-    System.out.println("正常終了しました。");
+    System.out.println("計算を開始します。");
+
+    System.out.println("計算完了。結果をメモ帳で表示します。");
+    ProcessBuilder pd = new ProcessBuilder(
+      "c:¥¥windows¥¥System32¥¥notepad.exe",
+      "calcreport.txt"
+    );
+    pd.start();
+
+    // boolean isErr = true;
+    // if (isErr){
+    //   System.out.println("データが壊れています。異常終了します。");
+    //   System.exit(1);
+    // }
+    // System.out.println("正常終了しました。");
   }
 }
