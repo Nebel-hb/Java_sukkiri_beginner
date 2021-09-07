1736c095f9c9b5184ec5a0471c0c9dc2bee762e0
Nebel-hb <nebel.hb@gmail.com>
2021-09-07 16:55:53 +0900

    Practical edition Java_5

--- Practical_edition/Java_5/Book.java
+++ Practical_edition/Java_5/Book.java
@@ -1,29 +1,29 @@
-// import org.apache.commons.lang3.builder.*;
+import org.apache.commons.lang3.builder.*;
 
-// public class Book implementsComparable<Book>, Cloneable {
-//   private String title;
-//   private Date publishDate;
-//   private String comment;
+public class Book implementsComparable<Book>, Cloneable {
+  private String title;
+  private Date publishDate;
+  private String comment;
 
-//   public int hashCode(){
-//     return HashCodeBuilder.reflectionHashCode(this);
-//   }
+  public int hashCode(){
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
 
-//   public boolean equals (Object o){
-//     return EqualsBuilder.reflectionEquals(this, o);
-//   }
+  public boolean equals (Object o){
+    return EqualsBuilder.reflectionEquals(this, o);
+  }
 
-//   public int compareTo(Book o){
-//     return CompareToBuilder.reflectionCompare(
-//       this, o, "comment", "title");
-//   }
+  public int compareTo(Book o){
+    return CompareToBuilder.reflectionCompare(
+      this, o, "comment", "title");
+  }
 
-//   public Book clone(){
-//     Book b = new Book();
-//     b.title = this.title;
-//     b.comment = this.comment;
-//     b.publishDate = (Date)this.publishDate.clone();
-//     return b;
-//   }
+  public Book clone(){
+    Book b = new Book();
+    b.title = this.title;
+    b.comment = this.comment;
+    b.publishDate = (Date)this.publishDate.clone();
+    return b;
+  }
 
-// }
+}
