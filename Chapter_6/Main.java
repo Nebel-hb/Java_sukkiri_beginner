db9822490f98cc1bd25d0b870b04b23d54fe81bf
Nebel-hb <nebel.hb@gmail.com>
2021-09-10 21:57:10 +0900

    Java Silver Chapter_6

--- Chapter_6/Main.java
+++ Chapter_6/Main.java
@@ -1,8 +1,16 @@
 public class Main{
   public static void main(String[] args){
-    Item a = new Item();
-    Item b = new Item();
-    b.setNum(20);
-    System.out.println(a.getNum());
+
+    Sample.num = 10;
+    Sample s = new Sample();
+    Sample s2 = new Sample();
+    s.num += 10;
+    s2.num = 30;
+    System.out.println(Sample.num);
+
+    // Item a = new Item();
+    // Item b = new Item();
+    // b.setNum(20);
+    // System.out.println(a.getNum());
   }
 }
--- Java_18/HelloServlet.java
+++ Java_18/HelloServlet.java
@@ -2,14 +2,14 @@ import java.io.*;
 import java.util.Date;
 import javax.servlet.http.*;
 
-// @WebServlet("/HelloServlet")
-// public class HelloServlet extends HttpServlet{
-//   protected void doGet(HttpServletRequest req, HttpServletResponse res)throws IOException{
-//     Date d = new Date();
-//     printWriter w = res.getWriter();
-//     res.setContentType("text/html");
-//     w.write("<html><body>");
-//     w.write("Today is " + d.toString());
-//     w.write("</body></html>");
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet{
+  protected void doGet(HttpServletRequest req, HttpServletResponse res)throws IOException{
+    Date d = new Date();
+    printWriter w = res.getWriter();
+    res.setContentType("text/html");
+    w.write("<html><body>");
+    w.write("Today is " + d.toString());
+    w.write("</body></html>");
   }
 }
