4b0d7442516dc11f8f369cf607712a41d1c22334
Nebel-hb <nebel.hb@gmail.com>
2021-09-12 20:28:19 +0900

    Java psql 8

--- Others/DBConnect1.java
+++ Others/DBConnect1.java
@@ -1,9 +1,6 @@
 import java.sql.*;
 
-public class Main {
-  // public static void main(String[] args) throws Exception {
-
-// public class DBConnect1 {
+public class DBConnect1 {
   public static void main(String[] args) throws Exception{
     Connection con;
     Statement st;
@@ -27,27 +24,5 @@ public class Main {
     rs.close();
     st.close();
     con.close();
-  // }
-// }
-    // Hero h = new Hero();
-    // h.name = "ミナト";
-    // h.hp = 100;
-
-    // Matango m1 = new Matango();
-    // m1.hp = 50;
-    // m1.suffix = 'A';
-
-    // Matango m2 = new Matango();
-    // m2.hp = 48;
-    // m2.suffix = 'B';
-
-    // System.out.println("勇者" + h.name + "を生み出した！");
-
-    // h.sit(5);
-    // h.slip();
-    // h.sit(25);
-    // m1.run();
-    // m2.run();
-    // h.run();
   }
 }
