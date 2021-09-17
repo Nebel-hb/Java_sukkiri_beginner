a93bc71ad484bfe582002ea4d1ab5f826510d417
Nebel-hb <nebel.hb@gmail.com>
2021-09-17 20:40:45 +0900

    Chapter_7

--- Chapter_7/A.java
+++ Chapter_7/A.java
@@ -1,5 +1,15 @@
-public interface A{
-  default void sample(){
-    System.out.println("sample");
+class A{
+  String val = "A";
+  void print(){
+    System.out.print(val);
   }
+
+// public interface A{
+  // default void sample(){
+  //   System.out.println("sample");
+  // }
+}
+
+class B extends A{
+  String val = "B";
 }
--- Chapter_7/Main.java
+++ Chapter_7/Main.java
@@ -1,7 +1,14 @@
 public class Main{
   public static void main(String[] args){
-    AbstractSample s = new ConcreteSample();
-    s.sample();
+    A a = new A();
+    A b = new B();
+    System.out.print(a.val);
+    System.out.print(b.val);
+    a.print();
+    b.print();
+
+    // AbstractSample s = new ConcreteSample();
+    // s.sample();
 
     // A a = new B();
     // a.sample();
