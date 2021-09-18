71300cea5d4cab319445972c5d6894c7bc6fe6b0
Nebel-hb <nebel.hb@gmail.com>
2021-09-18 23:21:10 +0900

    Chapter_7

--- Chapter_7/Main.java
+++ Chapter_7/Main.java
@@ -1,11 +1,15 @@
 public class Main{
   public static void main(String[] args){
-    A a = new A();
-    A b = new B();
-    System.out.print(a.val);
-    System.out.print(b.val);
-    a.print();
-    b.print();
+    Child child = new Child();
+    child.name = "sample";
+    System.out.println(child.getName());
+
+    // A a = new A();
+    // A b = new B();
+    // System.out.print(a.val);
+    // System.out.print(b.val);
+    // a.print();
+    // b.print();
 
     // AbstractSample s = new ConcreteSample();
     // s.sample();
