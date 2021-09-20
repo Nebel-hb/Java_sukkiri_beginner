b63785788d5ae078c59274c05d789640c49e99d5
Nebel-hb <nebel.hb@gmail.com>
2021-09-20 23:28:24 +0900

    Chapter_8

--- Chapter_8/Main.java
+++ Chapter_8/Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args){
-    Function f = (name) -> return "hello, " + name;
+    Function f = (name) -> { return "hello, " + name;};
     System.out.println(f.test("Lambda"));
   }
   private static interface Function{
