875d5cd506d000d6eff900a9d2a6980717c2c64c
Nebel-hb <nebel.hb@gmail.com>
2021-09-30 23:34:50 +0900

    Chapter_8

--- Chapter_8/Main.java
+++ Chapter_8/Main.java
@@ -1,9 +1,17 @@
+import java.util.function.*;
+
 public class Main{
   public static void main(String[] args){
-    Function f = (name) -> { return "hello, " + name;};
-    System.out.println(f.test("Lambda"));
-  }
-  private static interface Function{
-    String test (String name);
+
+    Function<String, Integer> func = (str) -> {
+      return Integer.parseInt(str);
+    };
+    System.out.println(func.apply("100") * 2);
+
+  //   Function f = (name) -> { return "hello, " + name;};
+  //   System.out.println(f.test("Lambda"));
+  // }
+  // private static interface Function{
+  //   String test (String name);
   }
 }
